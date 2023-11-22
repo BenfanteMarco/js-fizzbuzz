70a543cca1525d1a507a3dd734ab08463d0719d9
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2023-11-22 15:56:21 +0100

    [AD]conatiner-DOM-no-appendChild

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@
 // multiplo di 5 = 'Buzz';
 // multiplo di 15 = 'FizzBuzz' // multiplo sia di 3 che di 5;
 
+// DICHIARO LIST IN UNA CONST
+const list = document.querySelector('ul');
+
 for(let i=1; i<=100; i++){
 
     // resto3 = i % 3;
@@ -11,18 +14,30 @@ for(let i=1; i<=100; i++){
     // resto15 = i % 15 // multiplo sia di 3 che di 5;
 
     if(i%3 == 0 && i%5 == 0){
+        let li;
+        li = `<li id=num${i}>FizzBuzz</li>`;
+        list.innerHTML += li;
         // multiplo di 3 e 5 = 'FizzBuzz';
         console.log('FizzBuzz');
     }
     else if(i%5 == 0){
+        let li;
+        li = `<li id=num${i}>Buzz</li>`;
+        list.innerHTML += li;
         // multiplo di 5 = 'Buzz';
         console.log('Buzz');
     }
     else if(i%3 == 0){
+        let li;
+        li = `<li id=num${i}>Fizz</li>`;
+        list.innerHTML += li;
         // multiplo di 3 = 'Fizz';
         console.log('Fizz');
     }
     else{
+        let li;
+        li = `<li id=num${i}>${i}</li>`;
+        list.innerHTML += li;
         // nessun multiplo = 'numero';
         console.log(i);
     }
